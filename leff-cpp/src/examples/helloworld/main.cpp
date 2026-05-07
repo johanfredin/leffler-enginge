e5627ed75673a4573647d50c49f296cddd412527
Johan Fredin <johan.fredin.2@gu.se>
2026-05-07 20:22:24 +0200

    helloworld and new structure

---
+++ leff-cpp/src/examples/helloworld/main.cpp
@@ -0,0 +1,7 @@
+#include "LefflerApp.h"
+//
+// Created by johan on 2026-05-07.
+//
+int main() {
+    leffler::LefflerApp app("Leffe");
+}
---
+++ leff-cpp/src/lib/LefflerApp.cpp
@@ -0,0 +1,14 @@
+//
+// Created by johan on 2026-05-07.
+//
+
+#include "LefflerApp.h"
+
+#include <iostream>
+#include <ostream>
+
+namespace leffler {
+    LefflerApp::LefflerApp(const std::string &s) {
+        std::cout << "Hello " << s << std::endl;
+    }
+}
